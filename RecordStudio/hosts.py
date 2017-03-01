c18e174b017e159e412dd57a61cb6551d5d2e5d6
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2017-03-01 20:16:57 +0700

    flake8

--- RecordStudio/hosts.py
+++ RecordStudio/hosts.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host(r'recordstudio', settings.ROOT_URLCONF, name='main_host'),
-    host(r'about', 'about.urls', name='about'),
-
-)
+                         host(r'recordstudio', settings.ROOT_URLCONF, name='main_host'),
+                         host(r'about', 'about.urls', name='about'),
+                         )
